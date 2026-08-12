93265b0e645f3f7a9ca740341d2f5cde572290b8
mehran1355-sys <mehransababeh@gmail.com>
2026-08-12 16:07:34 +0330

    Create app.dart

---
+++ mobile_app/lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'config/theme.dart';
+import 'screens/home_screen.dart';
+
+class SupplyDemandApp extends StatelessWidget {
+  const SupplyDemandApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Supply & Demand Bot',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.darkTheme,
+      home: const HomeScreen(),
+    );
+  }
+}
